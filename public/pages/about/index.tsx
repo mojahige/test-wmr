c66b0237bf88a88d46730505063a4f10e156590f
mojahige <mojahige@gmail.com>
2021-04-20 23:04:37 +0900

    change type

--- public/pages/about/index.tsx
+++ public/pages/about/index.tsx
@@ -2,15 +2,15 @@ import styles from './style.module.css';
 
 interface Props {
   path?: string;
-  query?: string;
+  query?: unknown;
 }
 
 export default function About({ query }: Props): JSX.Element {
   return (
-  <section class={styles.about}>
-    <h1>About</h1>
-    <p>A page all about this website.</p>
-    <pre>{JSON.stringify(query)}</pre>
-  </section>
+    <section class={styles.about}>
+      <h1>About</h1>
+      <p>A page all about this website.</p>
+      <pre>{JSON.stringify(query)}</pre>
+    </section>
   )
 };
